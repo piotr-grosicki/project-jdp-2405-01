39bb35a3a685381d7380979fb8422a884f4f1e93
Paweł Gościak <pawgosciak@gmail.com>
2024-05-10 01:36:44 +0200

    GroupController - sample data
    Napisalem GroupController w pakiecie controller dla przykladowych, jeszcze nieistniejacych danych. W przypadku getAllGroups zwracana jest pusta lista. Zrobilem rowniez dto wedlug konwencji projektu.

---
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -0,0 +1,40 @@
+package com.kodilla.ecommercee.controller;
+
+import com.kodilla.ecommercee.dto.request.CreateGroupRequest;
+import com.kodilla.ecommercee.dto.request.UpdateGroupRequest;
+import com.kodilla.ecommercee.dto.response.GroupResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("shop/v1/group")
+public class GroupController {
+
+    @GetMapping
+    public ResponseEntity<List<GroupResponse>> getAllGroups() {
+        return ResponseEntity.ok(new ArrayList<>());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GroupResponse> getGroup(@PathVariable Long id) {
+        return ResponseEntity.ok(new GroupResponse(id, "Sample group name"));
+    }
+
+    @PostMapping
+    public ResponseEntity<GroupResponse> createGroup(@RequestBody CreateGroupRequest createGroupRequest) {
+        return ResponseEntity.ok(new GroupResponse(1L, createGroupRequest.name()));
+    }
+
+    @PutMapping
+    public ResponseEntity<GroupResponse> updateGroup(@RequestBody UpdateGroupRequest updateGroupRequest) {
+        return ResponseEntity.ok(new GroupResponse(updateGroupRequest.id(), updateGroupRequest.name()));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGroup(@PathVariable Long id) {
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/kodilla/ecommercee/dto/response/GroupResponse.java
@@ -0,0 +1,4 @@
+package com.kodilla.ecommercee.dto.response;
+
+public record GroupResponse() {
+}
