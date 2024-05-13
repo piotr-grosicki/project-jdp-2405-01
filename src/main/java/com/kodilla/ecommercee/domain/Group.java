693e01528262cc10f46862a14939124c561e32fe
Knutek1 <piotr.knuta@gmail.com>
2024-05-13 14:30:42 +0200

    JDP-240501-13 - Encja Group - implementacja

---
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -0,0 +1,32 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "Groups")
+public class Group {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Setter
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Product> productList = new ArrayList<>();
+
+}
---
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -0,0 +1,11 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Group;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
