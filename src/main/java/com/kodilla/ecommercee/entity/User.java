211488897780263ad5624357a55d8700dcf72f5a
Sebastian Lachut <seb.lachut@gmail.com>
2024-05-17 21:12:49 +0200

    Dodanie pola token do encji User

--- src/main/java/com/kodilla/ecommercee/entity/User.java
+++ src/main/java/com/kodilla/ecommercee/entity/User.java
@@ -29,6 +29,9 @@ public class User {
     @Column(name = "ADDRESS")
     private String address;
 
+    @Column(name = "TOKEN")
+    private Integer token;
+
     @OneToMany(targetEntity = Order.class,
             mappedBy = "user",
             fetch = FetchType.EAGER)
