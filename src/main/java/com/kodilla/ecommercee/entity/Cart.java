a9380ba1351f10cf1fe9718bf7dee2aa62f9bd21
Sebastian Lachut <seb.lachut@gmail.com>
2024-05-17 10:25:24 +0200

    Clear importow

--- src/main/java/com/kodilla/ecommercee/entity/Cart.java
+++ src/main/java/com/kodilla/ecommercee/entity/Cart.java
@@ -2,7 +2,6 @@ package com.kodilla.ecommercee.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
