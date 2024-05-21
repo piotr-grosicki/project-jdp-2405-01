e7c175e15b59039cf4356b34f85d86b1f700868c
Sebastian Lachut <seb.lachut@gmail.com>
2024-05-21 22:49:45 +0200

    Ujednolicenie kodu

--- src/main/java/com/kodilla/ecommercee/exception/GlobalHttpErrorHandler.java
+++ src/main/java/com/kodilla/ecommercee/exception/GlobalHttpErrorHandler.java
@@ -11,27 +11,27 @@ public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<Object> handleInvalidCredentialsException(InvalidCredentialsException invalidCredentialsException) {
-        return new ResponseEntity<>("Invalid credentials.", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(invalidCredentialsException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NullValueException.class)
     public ResponseEntity<Object> handleNullValueException(NullValueException nullValueException) {
-        return new ResponseEntity<>("Username, password or address is null.", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(nullValueException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UsernameAlreadyExistsException.class)
     public ResponseEntity<Object> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException usernameAlreadyExistsException) {
-        return new ResponseEntity<>("Username is already taken.", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(usernameAlreadyExistsException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Object> handlesUserNotFoundException(UserNotFoundException userNotFoundException) {
-        return new ResponseEntity<>("User could not be found.", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(userNotFoundException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<Object> handleUsernameNotFoundException(UsernameNotFoundException usernameNotFoundException) {
-        return new ResponseEntity<>("Username could not be found", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(usernameNotFoundException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
