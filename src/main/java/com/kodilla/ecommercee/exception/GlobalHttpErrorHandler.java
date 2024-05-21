d3858857b2278f09daa2f2965fde006fb85a7f6e
KayakOnWheels <115392859+KayakOnWheels@users.noreply.github.com>
2024-05-21 22:33:49 +0200

    Poprawiłem metody GlobalHttpErrorHandler tak, by wykorzystać message z wyjątków, które obsługują

--- src/main/java/com/kodilla/ecommercee/exception/GlobalHttpErrorHandler.java
+++ src/main/java/com/kodilla/ecommercee/exception/GlobalHttpErrorHandler.java
@@ -10,18 +10,18 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<Object> handleProductNotFoundException() {
-        return new ResponseEntity<>("Product with this id doesnt exist.", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException productNotFoundException) {
+        return new ResponseEntity<>(productNotFoundException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Object> handleUserNotFoundException() {
-        return new ResponseEntity<>("User with this id doesnt exist.", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException userNotFoundException) {
+        return new ResponseEntity<>(userNotFoundException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CartNotFoundException.class)
-    public ResponseEntity<Object> handleCartNotFoundException() {
-        return new ResponseEntity<>("Cart with this id doesnt exist.", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> handleCartNotFoundException(CartNotFoundException cartNotFoundException) {
+        return new ResponseEntity<>(cartNotFoundException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
